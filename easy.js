3ee09bf4e038d79b547568f00cd2b22fdb2b6e36
Michael Jung <jung.michaelh@gmail.com>
2022-11-14 16:45:55 -0800

    Complete hackerrank plusMinus problem

--- easy.js
+++ easy.js
@@ -1,4 +1,4 @@
-// TWO SUM
+// 1. TWO SUM
     // Given an array of integers nums and an integer target,
     //     return indices of the two numbers such that they add up to target.
     // You may assume that each input would have exactly one solution,
@@ -22,3 +22,21 @@
     // const nums = [2,7,11,15];
     // const target = 9;
     // console.log(twoSum(nums, target));
+
+
+
+// 26. REMOVE DUPLICATES FROM SORTED ARRAY:
+//     Given an integer array nums sorted in non-decreasing order,
+//       remove the duplicates in-place such that each unique element appears only once.
+//       The relative order of the elements should be kept the same.
+
+//     Since it is impossible to change the length of the array in some languages,
+//       you must instead have the result be placed in the first part of the array nums.
+//       More formally, if there are k elements after removing the duplicates,
+//       then the first k elements of nums should hold the final result.
+//       It does not matter what you leave beyond the first k elements.
+
+//     Return k after placing the final result in the first k slots of nums.
+
+//     Do not allocate extra space for another array.
+//     You must do this by modifying the input array in-place with O(1) extra memory.
--- hackerrank.js
+++ hackerrank.js
@@ -3,6 +3,23 @@
 
 // =============== WEEK 1 ============= //
 // ---------- 1. PLUS MINUS ---------- //
+function plusMinus(arr) {
+    const totalCount = arr.length;
+    let countNeg = 0;
+    let countZero = 0;
+    let countPos = 0;
+
+    arr.forEach(num => {
+        if (num < 0) countNeg++;
+        else if (num > 0) countPos++;
+        else countZero++;
+    });
+
+    console.log((countPos/totalCount).toFixed(6));
+    console.log((countNeg/totalCount).toFixed(6));
+    console.log((countZero/totalCount).toFixed(6));
+}
+
 // ---------- 2. MINI-MAX SUM ---------- //
 // ---------- 3. TIME CONVERSION ---------- //
 // ---------- 4. SPARSE ARRAYS ---------- //
