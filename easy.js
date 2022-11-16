b87613660632f68d92a017e4bfbfb22d3b257095
Michael Jung <jung.michaelh@gmail.com>
2022-11-16 14:15:44 -0800

    Solve Roman to Integer

--- easy.js
+++ easy.js
@@ -49,3 +49,55 @@
 
     // const x = 121;
     // console.log(isPalindrome(x));
+
+
+// 10. ROMAN TO INTEGER
+    // Given a roman numeral, convert it to an integer.
+    function romanToInt(s) {
+        const romans = {
+            I: 1,
+            V: 5,
+            X: 10,
+            L: 50,
+            C: 100,
+            D: 500,
+            M: 1000
+        }
+
+        let converted = 0;
+        for (let i = 0; i < s.length; i++) {
+            const currChar = s[i];
+            const nextChar = s[i + 1];
+
+            if (currChar === "I") {
+                if (nextChar && nextChar !== "I") {
+                    converted += romans[nextChar] - 1;
+                    i++;
+                } else {
+                    converted += romans[currChar];
+                }
+            }
+            else if (currChar === "X") {
+                if (nextChar && (nextChar !== "I" && nextChar !== "V" && nextChar !== "X")) {
+                    converted += romans[nextChar] - 10;
+                    i++;
+                } else {
+                    converted += romans[currChar];
+                }
+            }
+            else if (currChar === "C") {
+                if (nextChar && (nextChar === "D" || nextChar === "M")) {
+                    converted += romans[nextChar] - 100;
+                    i++;
+                } else {
+                    converted += romans[currChar];
+                }
+            }
+            else converted += romans[currChar];
+        }
+
+        return converted;
+    }
+
+    // const s = "DCXXI"; // 621
+    // console.log(romanToInt(s));
