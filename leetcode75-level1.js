21ce9a5988d9ba454d8210dc2321cbdf5d52c772
Michael Jung <jung.michaelh@gmail.com>
2022-11-16 19:48:05 -0800

    Create templates for leetcode75

---
+++ leetcode75-level1.js
@@ -0,0 +1,63 @@
+// ================================================= //
+// ==================== LEVEL 1 ==================== //
+// ================================================= //
+
+// ========== DAY 1 ========== //
+// 1480. Running Sum of 1d Array
+// 724. Find Pivot Index
+
+// ========== DAY 2 ========== //
+// 205. Isomorphic Strings
+// 392. Is Subsequence
+
+// ========== DAY 3 ========== //
+// 21. Merge Two Sorted Lists
+// 206. Reverse Linked List
+
+// ========== DAY 4 ========== //
+// 876. Middle of the Linked List
+// 142. Linked List Cycle II
+
+// ========== DAY 5 ========== //
+// 121. Best Time to Buy and Sell Stock
+// 409. Longest Palindrome
+
+// ========== DAY 6 ========== //
+// 589. N-ary Tree Preorder Traversal
+// 102. Binary Tree Level Order Traversal
+
+// ========== DAY 7 ========== //
+// 704. Binary Search
+// 278. First Bad Version
+
+// ========== DAY 8 ========== //
+// 98. Validate Binary Search Tree
+// 235. Lowest Common Ancestor of a Binary Search Tree
+
+// ========== DAY 9 ========== //
+// 733. Flood Fill
+// 200. Number of Islands
+
+// ========== DAY 10 ========== //
+// 509. Fibonacci Number
+// 70. Climbing Stairs
+
+// ========== DAY 11 ========== //
+// 746. Min Cost Climbing Stairs
+// 62. Unique Paths
+
+// ========== DAY 12 ========== //
+// 438. Find All Anagrams in a String
+// 424. Longest Repeating Character Replacement
+
+// ========== DAY 13 ========== //
+// 1. Two Sum
+// 299. Bulls and Cows
+
+// ========== DAY 14 ========== //
+// 844. Backspace String Compare
+// 394. Decode String
+
+// ========== DAY 15 ========== //
+// 1046. Last Stone Weight
+// 692. Top K Frequent Words
