ef7207231efc8322589de3b159db5fb151e5768f
Michael Jung <jung.michaelh@gmail.com>
2022-12-10 10:48:43 -0800

    Solve House Robber

--- medium.js
+++ medium.js
@@ -8,6 +8,34 @@
     //   except the number 0 itself.
 
 
+// 198. House Robber
+const rob = (nums) => {
+    if (!nums.length) return 0;
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
+
+    // let maxAtTwoBefore = nums[0];
+    // let maxAtOneBefore = Math.max(nums[0], nums[1]);
+
+    // for (let i = 2; i < nums.length; i++) {
+    //     const maxAtCurrent = Math.max(nums[i] + maxAtTwoBefore, maxAtOneBefore);
+
+    //     maxAtTwoBefore = maxAtOneBefore;
+    //     maxAtOneBefore = maxAtCurrent;
+    // }
+
+    // return maxAtOneBefore;
+
+    const state = [];
+    state[0] = nums[0];
+    state[1] = Math.max(state[0], nums[1]);
+    for (let i = 2; i < nums.length; i++) {
+        state[i] = Math.max(state[i-1], state[i-2] + nums[i]);
+    }
+    return state[nums.length-1];
+};
+
+
 // // 809. Expressive Words
 // const expressiveWords = (s, words) => {
 //     /* PSEUDOCODE:
