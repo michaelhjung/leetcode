28ae53cc5b0a987691cc73409d0b90d0302deb50
Michael Jung <jung.michaelh@gmail.com>
2022-12-15 20:45:52 -0800

    Solve First Bad Version

--- grind75.js
+++ grind75.js
@@ -212,6 +212,23 @@ const hasCycle = head => {
 
 // ===== Week 2 ===== //
 // First Bad Version
+const isBadVersion = () => {
+    return n => {
+        let left = 0;
+        let right = n;
+
+        while (left <= right) {
+            const mid = left + Math.floor((right - left) / 2);
+
+            if (isBadVersion(mid)) {
+                if (isBadVersion(mid - 1)) right = mid - 1;
+                else return mid;
+            }
+            else left = mid + 1;
+        }
+    }
+}
+
 // Ransom Note
 // *Climbing Stairs*
 // Longest Palindrome
