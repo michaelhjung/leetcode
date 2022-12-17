976de232d477a301a39a15567e02a9f06f03ec9d
Michael Jung <jung.michaelh@gmail.com>
2022-12-16 22:52:38 -0800

    Solve Insert Interval

--- grind75.js
+++ grind75.js
@@ -379,6 +379,30 @@ const maxSubArray = nums => {
 
 // ===== Week 3 ===== //
 // *Insert Interval*
+const insert = (intervals, newInterval) => {
+    let [start, end] = newInterval;
+    let left = [];
+    let right = [];
+
+    for (const interval of intervals) {
+        const [first, last] = interval;
+
+        // current interval is smaller than newInterval
+        if (last < start) left.push(interval);
+
+        // current interval is larger than newInterval
+        else if (first > end) right.push(interval);
+
+        // there is a overlap
+        else {
+            start = Math.min(start, first);
+            end = Math.max(end, last);
+        }
+    }
+
+    return [...left, [start, end], ...right];
+};
+
 // 01 Matrix
 // K Closest Points to Origin
 // *Longest Substring Without Repeating Characters*
