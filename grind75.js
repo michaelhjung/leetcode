3f921554b485669b0ea3e1d62974c0aab8c6121c
Michael Jung <jung.michaelh@gmail.com>
2022-12-27 10:45:17 -0800

    Solve Min Stack

--- grind75.js
+++ grind75.js
@@ -618,6 +618,31 @@ const productExceptSelf = nums => {
 };
 
 // Min Stack
+class MinStack {
+    constructor() {
+        this.stack = [];
+    }
+
+    push(val) {
+        this.stack.push({
+            value: val,
+            min: this.stack.length === 0 ? val : Math.min(val, this.getMin())
+        });
+    }
+
+    pop() {
+        this.stack.pop();
+    }
+
+    top() {
+        return this.stack[this.stack.length - 1].value;
+    }
+
+    getMin() {
+        return this.stack[this.stack.length - 1].min;
+    }
+};
+
 // *Validate Binary Search Tree*
 // *Number of Islands*
 // Rotting Oranges
