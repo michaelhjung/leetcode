fc56374d8ac18b29403e6a74c3ab464cc023b697
Michael Jung <jung.michaelh@gmail.com>
2022-12-28 22:10:20 -0800

    Solve Validate Binary Search Tree

--- grind75.js
+++ grind75.js
@@ -644,6 +644,11 @@ class MinStack {
 };
 
 // *Validate Binary Search Tree*
+const isValidBST = (root, min=-Infinity, max=Infinity) => {
+    if (!root) return true;
+    if (root.val <= min || root.val >= max) return false;
+    return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+}
 // *Number of Islands*
 // Rotting Oranges
 
