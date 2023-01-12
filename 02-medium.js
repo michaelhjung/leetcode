dc2149e8f39ec7778f10af6cfda1cdb89060edd5
Michael Jung <jung.michaelh@gmail.com>
2023-01-12 14:43:27 -0800

    Solve Subsets

--- 02-medium.js
+++ 02-medium.js
@@ -86,6 +86,31 @@ const rotate = matrix => {
 };
 
 
+// 78. Subsets
+const subsets = nums => {
+    const res = [];
+    const subset = [];
+
+    const dfs = i => {
+        if (i >= nums.length) {
+            res.push(subset.slice(0));
+            return;
+        }
+
+        // decision to include nums[i]:
+        subset.push(nums[i]);
+        dfs(i + 1);
+
+        // decision to NOT include nums[i]:
+        subset.pop();
+        dfs(i + 1);
+    }
+
+    dfs(0);
+    return res;
+}
+
+
 // 91. Decode Ways
 const numDecodings = s => {
     if (!s || s.length === 0) return 0;
